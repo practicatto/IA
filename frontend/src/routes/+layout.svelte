5dd5d5ed6a04048e43a898421d074ca2c937b7d4
Joangie Márquez <practicatto@gmail.com>
2022-08-23 05:57:11 -0500

    dummy 3D viewer

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '../app.css';
+	import Header from '$lib/components/layout/Header.svelte';
+</script>
+
+<Header />
+
+<div class="container mx-auto my-6 max-w">
+	<slot />
+</div>
